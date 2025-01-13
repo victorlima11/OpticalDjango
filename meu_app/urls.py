4c972d8436b74c97bc01d9d24aa702cd64410c4f
victorlima11 <victorconta1009@gmail.com>
2025-01-13 18:51:57 -0300

    adicionando login ao urls

--- meu_app/urls.py
+++ meu_app/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
 ]
 
 
