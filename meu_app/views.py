14b39da7d998c9a9e24bdf067ea10c125c7b3bec
Joaolucasos169 <lukasstefan03@gmail.com>
2025-01-13 18:49:35 -0300

    Login

--- meu_app/views.py
+++ meu_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+def login_view(request):
+    if request.method == 'POST':
+        # Aqui você pode tratar a autenticação do usuário
+        username = request.POST['username']
+        password = request.POST['password']
+        # Autenticação do usuário pode ser realizada aqui
+    return render(request, 'login.html')
+
 # Create your views here.
