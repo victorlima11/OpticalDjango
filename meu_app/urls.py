fced4976ac86c2b46b61545671910a6e858bee50
Lduarte123 <duare744@gmail.com>
2025-01-15 20:38:02 -0300

    termindando home

--- meu_app/urls.py
+++ meu_app/urls.py
@@ -5,15 +5,12 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
-<<<<<<< HEAD
     path('sobre/', views.sobre, name='sobre'),
     path('contato/', views.contato, name='contato'),
-=======
     # path('carrinho/', views.exibir_carrinho, name='exibir_carrinho'),
     # path('adicionar/<int:produto_id>/', views.adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
     # path('remover/<int:produto_id>/', views.remover_do_carrinho, name='remover_do_carrinho'),
     path('cadastro/', views.cadastro_usuario, name='cadastro_usuario'),
->>>>>>> 6cc00cf810926e457e9f41f3aac15d4ec34c4277
 ]
 
 
