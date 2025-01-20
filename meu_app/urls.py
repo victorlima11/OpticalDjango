cac243a91e4d9e9d3a1a84afe4b787966ff53ff3
cauaenzo <cauaenzo07@gmail.com>
2025-01-20 19:11:27 -0300

    gestão de erros

--- meu_app/urls.py
+++ meu_app/urls.py
@@ -1,21 +1,20 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView, LogoutView
 
-# app_name = 'meu_app'
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Página inicial do app
-    path('contatos/', views.contatos, name='contato'),
+    path('', views.home, name='home'),
+    path('catalogo', views.catalogo, name='catalogo'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('sobre/', views.sobre, name='sobre'),
-    path('endereco/', views.endereco, name='endereco'),
-    path('blog/', views.blog, name='blog'),
-    path('parceiros/', views.parceiros, name='parceiros'),
-    path('cadastrar_cliente/', views.cadastrar_cliente, name='cadastrar_cliente'),
-    path('cadastrar_produto/', views.cadastrar_produto, name='cadastrar_produto'),
-    path('cadastrar_pedido/', views.cadastrar_pedido, name='cadastrar_pedido'),
-    path('listar_clientes', views.listar_clientes, name='listar_clientes'),
-    path('clientes/excluir/<int:cliente_id>/', views.excluir_cliente, name='excluir_cliente'),
+    path('contato/', views.contato, name='contato'),
+    # path('carrinho/', views.exibir_carrinho, name='exibir_carrinho'),
+    # path('adicionar/<int:produto_id>/', views.adicionar_ao_carrinho, name='adicionar_ao_carrinho'),
+    # path('remover/<int:produto_id>/', views.remover_do_carrinho, name='remover_do_carrinho'),
+    path('cadastro/', views.cadastro_usuario, name='cadastro_usuario'),
 ]
+
 
 
 
