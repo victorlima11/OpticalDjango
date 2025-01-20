89c6b5094bdd197ca27153bf016b428d179212dc
cauaenzo <cauaenzo07@gmail.com>
2025-01-20 18:59:03 -0300

    refatoração

--- meu_app/urls.py
+++ meu_app/urls.py
@@ -4,14 +4,18 @@ from . import views
 # app_name = 'meu_app'
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='home'),  # Página inicial do app
+    path('contatos/', views.contatos, name='contato'),
     path('sobre/', views.sobre, name='sobre'),
-    path('catalogo/', views.catalogo, name='catalogo'),
-    path('contato/', views.contato, name='contato'),
-    path('carrinho/', views.carrinho, name='carrinho'),
-    path('cadastro/', views.cadastro_usuario, name='cadastro_usuario'),
+    path('endereco/', views.endereco, name='endereco'),
+    path('blog/', views.blog, name='blog'),
+    path('parceiros/', views.parceiros, name='parceiros'),
+    path('cadastrar_cliente/', views.cadastrar_cliente, name='cadastrar_cliente'),
+    path('cadastrar_produto/', views.cadastrar_produto, name='cadastrar_produto'),
+    path('cadastrar_pedido/', views.cadastrar_pedido, name='cadastrar_pedido'),
+    path('listar_clientes', views.listar_clientes, name='listar_clientes'),
+    path('clientes/excluir/<int:cliente_id>/', views.excluir_cliente, name='excluir_cliente'),
 ]
-
 
 
 
